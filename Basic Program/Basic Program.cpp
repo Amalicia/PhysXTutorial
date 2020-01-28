bb5e0bbe0b61451bb4cd7ae26a0c6723b260c080
Lottie Bryant <amaliciabryant@gmail.com>
2020-01-28 10:54:02 +0000

    what was done in the workshop

--- Basic Program/Basic Program.cpp
+++ Basic Program/Basic Program.cpp
@@ -18,6 +18,7 @@ PxPvd*  pvd = 0;
 //simulation objects
 PxScene* scene;
 PxRigidDynamic* box;
+PxRigidStatic* box18;
 PxRigidStatic* plane;
 
 ///Initialise PhysX objects
@@ -98,19 +99,55 @@ void InitScene()
 	scene->setGravity(PxVec3(0.f, -9.81f, 0.f));
 
 	//materials
-	PxMaterial* default_material = physics->createMaterial(0.f, 0.f, 0.f);   //static friction, dynamic friction, restitution
+	PxMaterial* default_material = physics->createMaterial(0.f, .07f, .5f);   //static friction, dynamic friction, restitution
 
 	//create a static plane (XZ)
 	plane = PxCreatePlane(*physics, PxPlane(PxVec3(0.f, 1.f, 0.f), 0.f), *default_material);
 	scene->addActor(*plane);
 
 	//create a dynamic actor and place it 10 m above the ground
-	box = physics->createRigidDynamic(PxTransform(PxVec3(0.f, 10.f, 0.f)));
+	box = physics->createRigidDynamic(PxTransform(PxVec3(0.f, 101.f, 0.f)));
 	//create a box shape of 1m x 1m x 1m size (values are provided in halves)
 	box->createShape(PxBoxGeometry(.5f, .5f, .5f), *default_material);
 	//update the mass of the box
 	PxRigidBodyExt::updateMassAndInertia(*box, 1.f); //density of 1kg/m^3
+
 	scene->addActor(*box);
+
+	box = physics->createRigidDynamic(PxTransform(PxVec3(5.f, 101.f, 0.f)));
+	box->createShape(PxBoxGeometry(.5f, .5f, .5f), *default_material);
+	PxRigidBodyExt::updateMassAndInertia(*box, 1.f); //density of 1kg/m^3
+	scene->addActor(*box);
+
+	box = physics->createRigidDynamic(PxTransform(PxVec3(10.f, 101.f, 0.f)));
+	box->createShape(PxBoxGeometry(.5f, .5f, .5f), *default_material);
+	PxRigidBodyExt::updateMassAndInertia(*box, 1.f); //density of 1kg/m^3
+	scene->addActor(*box);
+
+
+	 //create a dynamic actor and place it 10 m above the ground
+	box18 = physics->createRigidStatic(PxTransform(PxVec3(0.f, 1.f, 0.f)));
+	//create a box shape of 1m x 1m x 1m size (values are provided in halves)
+	box18->createShape(PxBoxGeometry(1.f, 1.f, 1.f), *default_material);
+	//update the mass of the box
+	//PxRigidBodyExt::updateMassAndInertia(*box18, 1.f); //density of 1kg/m^3
+	//box18->setRigidBodyFlag(PxRigidBodyFlag::, true);
+
+	box = physics->createRigidDynamic(PxTransform(PxVec3(5.f, 1.f, 0.f)));
+	box->createShape(PxBoxGeometry(1.f, 1.f, 1.f), *default_material);
+	PxRigidBodyExt::updateMassAndInertia(*box, 1.f);
+
+	scene->addActor(*box);
+
+	box = physics->createRigidDynamic(PxTransform(PxVec3(10.f, 1.f, 0.f)));
+	box->createShape(PxBoxGeometry(1.f, 1.f, 1.f), *default_material);
+	PxRigidBodyExt::updateMassAndInertia(*box, 1.f);
+	box->setRigidBodyFlag(PxRigidBodyFlag::eKINEMATIC, true);
+
+
+	scene->addActor(*box);
+
+	scene->addActor(*box18);
 }
 
 /// Perform a single simulation step
@@ -137,6 +174,16 @@ int main()
 	PxReal delta_time = 1.f/60.f;
 
 	//simulate until the 'Esc' is pressed
+
+	float count = 0;
+
+	cout << box->getMass() << endl;
+
+
+	//box->addForce(PxVec3(100.f, 0, 0));
+
+	bool checkBol = false;
+
 	while (!GetAsyncKeyState(VK_ESCAPE))
 	{
 		//'visualise' position and velocity of the box
@@ -146,12 +193,32 @@ int main()
 			", y=" << position.y << ", z=" << position.z << ",  ";
 		cout << setiosflags(ios::fixed) << setprecision(2) << "vx=" << velocity.x << 
 			", vy=" << velocity.y << ", vz=" << velocity.z << endl;
+		cout << "counter: " << count << endl;
 
+		//if (velocity.y < -0.01f)
+		{
+			count ++;
+		}
+
+		if (box->isSleeping())
+		{
+			cout << "\n\n\n\n\n SLeepyboiii";
+		}
+
+
+
+
+		if (position.x >= 10)
+		{
+			velocity.x = 0;
+			box->setLinearVelocity(velocity);
+		}
+		
 		//perform a single simulation step
 		Update(delta_time);
 		
 		//introduce 100ms delay for easier visual analysis of the results
-		Sleep(100);
+		Sleep(1);
 	}
 
 	//Release all resources
